6a853a25f541c2ff77369d2135cb6440bb4a5031
Yash Kamaat <yashtkamaat@gmail.com>
2021-10-20 17:45:13 -0400

    Completed

---
+++ script.js
@@ -0,0 +1,42 @@
+// 1a
+const jPDinos = ["Velociraptors", "Tyrannosaurus Rex", "Dilophosaurus"];
+// 1b
+function seeDinos([v, ...t]) {
+        console.log(v);
+        console.log(t);
+}
+seeDinos(jPDinos);
+
+// 2a
+const jPCharacters = {
+    alanGrant: "Sam Neill",
+    ellieSattler: "Laura Dern",
+    ianMalcolm: "Jeff Goldblum"
+};
+// 2b
+ seeCharacters = ({ellieSattler}) => {
+    console.log(ellieSattler);
+}
+seeCharacters(jPCharacters);
+
+// Bonus
+// 3a
+const jurassicParkMovies = [{
+    one: "Jurassic Park",
+    two: "The Lost World: Jurassic Park",
+    three: "Jurassic Park III"
+}, {
+    four: "Jurassic World",
+    five: "Jurassic World: Fallen Kingdom",
+    six: "Jurassic World: Dominion"
+}];
+// 3b
+function seeJPMovies([{one, two, three}, {four, five, six}]){
+    console.log(one);
+    console.log(two);
+    console.log(three);
+    console.log(four);
+    console.log(five);
+    console.log(six);
+}
+seeJPMovies(jurassicParkMovies);
